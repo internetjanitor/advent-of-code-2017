1ca6a0a8f16f8d17d98daf88050909a3c9b133ec
Eric Miller <internetjanitor@gmail.com>
2017-12-03 14:44:38 -0500

    Update spiral_memory.py

--- day03/spiral_memory.py
+++ day03/spiral_memory.py
@@ -5,23 +5,49 @@ from collections import defaultdict
 goal=312051
 
 coords = defaultdict(dict)
-digit = defaultdict(dict)
+
+def adjacent_sum(x, y):
+    sum = 0
+    if coords[x].get(y-1, {}):
+        sum += coords[x][y-1]
+    if coords[x].get(y+1, {}):
+        sum += coords[x][y+1]
+    if coords.get(x-1, {}):
+        if coords[x-1].get(y, {}):
+            sum += coords[x-1][y]
+        if coords[x-1].get(y-1, {}):
+            sum += coords[x-1][y-1]
+        if coords[x-1].get(y+1, {}):
+            sum += coords[x-1][y+1]
+    if coords.get(x+1, {}):
+        if coords[x+1].get(y, {}):
+            sum += coords[x+1][y]
+        if coords[x+1].get(y-1, {}):
+            sum += coords[x+1][y-1]
+        if coords[x+1].get(y+1, {}):
+            sum += coords[x+1][y+1]
+    return sum
 
 num = 1
 x,y = 0,0
 
 coords[x][y] = num
-digit[num]['x'] = x
-digit[num]['y'] = y
+notfound = True
 
 x += 1 # START RIGHT
 
 while num < goal:
     num += 1
-    coords[x][y] = num
-    digit[num]['x'] = x
-    digit[num]['y'] = y
 
+    sum = adjacent_sum(x, y)
+    if sum > goal and notfound:
+        print sum
+        notfound = False
+    coords[x][y] = sum
+
+    if num == goal:
+        print abs(x) + abs(y)
+    
     if y in coords.get(x-1, {}) and y+1 not in coords[x] and y-1 not in coords[x]:
         y += 1 # UP
     elif y-1 in coords[x] and y+1 not in coords[x] and y not in coords.get(x-1, {}):
@@ -36,5 +62,3 @@ while num < goal:
         x += 1 # RIGHT
     elif y in coords.get(x-1, {}) and y+1 not in coords[x] and y-1 in coords[x]:
         y += 1 # UP
-
-print abs(digit[goal]['x']) + abs(digit[goal]['y'])
